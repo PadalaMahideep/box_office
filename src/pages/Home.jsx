f81caf668c2cb3090ca3a1a4758f7c655cf3a28d
Mahideep <mahipadala206@gmail.com>
2023-05-23 01:17:11 +0530

    added search box for searching shows and movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+
 const Home = () => {
+  const [searchstr, setsearchstr] = useState('');
+
+  const onsearchstrchange = ev => {
+    setsearchstr(ev.target.value);
+  };
+
+  const onsearch = async ev => {
+    ev.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchstr}`
+    );
+    const body = await response.json();
+  };
   return (
     <div>
-      <h2>Home page</h2>
+      <form onSubmit={onsearch}>
+        <h2>Home page</h2>
+        <div>{searchstr}</div>
+        <input type="text" value={searchstr} onChange={onsearchstrchange} />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 };
