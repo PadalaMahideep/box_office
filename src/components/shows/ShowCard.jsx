f376882d3be837e506b04793f62ba4880dd66bcc
Mahideep <mahipadala206@gmail.com>
2023-06-25 14:49:37 +0530

    added fade-in animation to FlexGrid . added useref to manipulate animation to start button.

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -6,11 +6,29 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../common/Searchcard';
 import { StarIcon } from '../common/Staricon';
+import { useRef } from 'react';
+
 const ShowCard = ({ name, image, id, summary, onstaredMe, isStared }) => {
   const summaryshort = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, ' ') + '.....'
     : 'NoDiscription';
 
+  const starBtnRef = useRef();
+  const handlestartclick = _ => {
+    onstaredMe(id);
+
+    const StarbtnEl = starBtnRef.current;
+
+    if (!starBtnRef) {
+      return;
+    }
+
+    if (isStared) {
+      StarbtnEl.classList.remove('animate');
+    } else {
+      StarbtnEl.classList.add('animate');
+    }
+  };
   return (
     <SearchCard>
       <SearchImgWrapper>
@@ -23,7 +41,12 @@ const ShowCard = ({ name, image, id, summary, onstaredMe, isStared }) => {
       <p>{summaryshort}</p>
       <ActionSection>
         <Link to={`/show/${id}`}>ReadMore </Link>
-        <StarBtn type="button" onClick={() => onstaredMe(id)}>
+        <StarBtn
+          ref={starBtnRef}
+          type="button"
+          onClick={handlestartclick}
+          // className={isStared && 'animate'} used useref to manipulate animate
+        >
           <StarIcon active={isStared} />
 
           {/* {isStared ? 'unstar me' : 'star me'} */}
@@ -49,7 +72,6 @@ const ActionSection = styled.div`
     }
   }
 `;
-
 const StarBtn = styled.button`
   outline: none;
   border: 1px solid #8e8e8e;
@@ -61,5 +83,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
